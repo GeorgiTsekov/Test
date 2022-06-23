3268ab5de2ed67fa0022d3a24f79ffa113064d49
Georgi Tsekov <georgi_tsekov@abv.bg>
2022-06-24 00:15:28 +0300

    add Constants

---
+++ BurnedAcres/BurnedAcresTask/BurnedAcresTask/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace BurnedAcres
+{
+    public static class Constants
+    {
+        public const string STOP = "stop";
+        public const string TWO_INTEGERS_WITH_SPACE = "The coordinates shoult be two integers separated by space!";
+        public const string OUT_OF_RANGE = "Current coordinates are outside of the land!";
+        public const string ADDED_ALREADY = "You can't add this coordinates, they were added already!";
+        public const string SUCCESSFULL = "You have just added new coordinates to the land!";
+        public const string NULL_OR_WHITESPACE = "The number cannot be null or whitespace!";
+        public const string NOT_INTEGER = "The number shout be integer!";
+        public const string NOT_POSITIVE = "The number shout be positive!";
+        public const string ADD_POSITIVE_INTEGER = "Add positive integer for {0} of the land: ";
+        public const string ADD_COORDINATES = "Add two coordinates like: '0 0', or write {0}: ";
+        public const string NO_FIRES = "No fires on the land";
+    }
+}
