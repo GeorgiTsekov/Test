45a8cf8dcd03e35a42b5f5d69fecc7bb58ada4ae
Georgi Tsekov <georgi_tsekov@abv.bg>
2022-06-24 00:17:29 +0300

    Add IntegerValidationTests

---
+++ BurnedAcres/BurnedAcresTask/BurnedAcres.Tests/IntegerValidationTests.cs
@@ -0,0 +1,37 @@
+﻿namespace BurnedAcres.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class IntegerValidationTests
+    {
+        [TestCase("333")]
+        [TestCase("1")]
+        [TestCase("55")]
+        public void IsValidMethodShouldReturnIntegerForValidInteger(string number)
+        {
+            var integerValidation = new IntegerValidation();
+            int result = integerValidation.IsValid(number);
+            Assert.AreEqual(result, int.Parse(number));
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void IsValidMethodShouldReturnMinusOneForNullOrWhiteSpace(string number)
+        {
+            var integerValidation = new IntegerValidation();
+            int result = integerValidation.IsValid(number);
+            Assert.AreEqual(result, -1);
+        }
+
+        [TestCase("asdf")]
+        [TestCase("13.51")]
+        [TestCase("-313.51")]
+        public void IsValidMethodShouldReturnMinusOneForDecimalOrString(string number)
+        {
+            var integerValidation = new IntegerValidation();
+            int result = integerValidation.IsValid(number);
+            Assert.AreEqual(result, -1);
+        }
+    }
+}
